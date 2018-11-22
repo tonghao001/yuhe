0dd292a7ba2802d74848388cf89328b3bd793f24
indation_elina_guo <elina.guo@indation.com>
2018-11-22 18:58:33 +0800

    添加图标在考勤页面

--- src/pages/home/childManage/childCheckOn/childCheckOn.module.ts
+++ src/pages/home/childManage/childCheckOn/childCheckOn.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { ChildCheckOn } from "./childCheckOn";
+import { ChartModule } from 'angular-highcharts';
 
 @NgModule({
-  declarations:[
+  declarations: [
     ChildCheckOn
   ],
-  imports:[
-    IonicPageModule.forChild(ChildCheckOn)
+  imports: [
+    IonicPageModule.forChild(ChildCheckOn),
+    ChartModule
   ],
-  entryComponents:[
+  entryComponents: [
     ChildCheckOn
   ]
 })
-export class ChildCheckOnPageModule {};
+export class ChildCheckOnPageModule { };
--- src/pages/home/childManage/childCheckOn/childCheckOn.ts
+++ src/pages/home/childManage/childCheckOn/childCheckOn.ts
@@ -1,5 +1,6 @@
 import { IonicPage } from 'ionic-angular';
 import { Component } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
 
 @IonicPage({
@@ -9,5 +10,49 @@ import { Component } from '@angular/core';
   templateUrl: 'childCheckOn.html'
 })
 export class ChildCheckOn {
+  chart: Chart = new Chart({
+    chart: {
+      type: 'pie'
+    },
+    title: {
+      text: ''
+    },
+
+    credits: {
+      enabled: false
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: false
+        },
+        showInLegend: true
+      }
+    },
+    series: [{
+      name: 'Brands',
+      data: [{
+        name: '实到',
+        y: 20,
+        sliced: true,
+        selected: true,
+        color: '#df56ff'
+      }, {
+        name: '应到',
+        y: 25,
+        color: '#08f8f5'
+      }, {
+        name: '请假',
+        y: 12,
+        color: '#71b9fd'
+      }, {
+        name: '未签到',
+        y: 43,
+        color: '#7d81ff'
+      }]
+    }]
+  });
 
 }
