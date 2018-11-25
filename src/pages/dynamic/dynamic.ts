e245bb5d05257dda8a8cbda6dbb695cc5eabf683
indation_elina_guo <elina.guo@indation.com>
2018-11-25 15:45:43 +0800

    迁移大葱页面入口到动态

--- src/pages/dynamic/dynamic.ts
+++ src/pages/dynamic/dynamic.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'dynamic.html'
 })
 export class DynamicPage { 
+  constructor(public nav: NavController) {
+      
+  }
+  announcementClick() {
+    this.nav.push('app-home-announcement');
+  }
+  princeipalEmailClick() {
+    this.nav.push('app-home-principal-email');
+  }
+  myApprovalClick() {
+    this.nav.push('app-home-my-approval');
+  }
+  salaryButtonClick() {
+    this.nav.push('app-home-salary');
+
+  }
 }
