91daac4fed233872e2deefa0b849b4b3f5057949
刘聪 <conorliu@163.com>
2018-12-02 10:58:34 +0800

    update:  整理文件夹

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -5,14 +5,14 @@ import { StartByMePageModule } from './approval/startByMe/startByMe.module';
 import { MyApprovalPageModule } from './approval/myApproval/myApproval.module';
 import { SalaryPageModule } from './salary/salary.module';
 import { PrincipalEmailPageModule } from './principalEmail/principalEmail.module';
-import { EditAnnouncementPageModule } from './editAnnouncement/editAnnouncement.module';
-import { AnnounceDetailsPageModule } from './announcement/announce-details.module';
+import { EditAnnouncementPageModule } from './announcement/editAnnouncement/editAnnouncement.module';
+import { AnnounceDetailsPageModule } from './announcement/annluncementDetails/announce-details.module';
 import { AnnouncementPageModule } from './announcement/announcement.module';
 import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { HomePage } from "./home";
 import { ChildManageModule } from "./childManage/childManage.module";
-import { FeedbackEmailPageModule } from './feedbackEmail/feedbackEmail.module';
+import { FeedbackEmailPageModule } from './principalEmail/feedbackEmail/feedbackEmail.module';
 import {ChartModule} from 'angular-highcharts';
 import { ApprovalDetailsPageModule } from './approval/approvalDetails/approvalDetails.module';
 import { kindergartenOverviewModule } from "./kindergartenOverview/kindergartenOverview.module";
@@ -23,7 +23,6 @@ import { CheckInPageModule } from './checkIn/checkInPage/checkIn.module';
 import { ApprovalPageModule } from './approval/approvalPage/approvalPage.module';
 import { ProcurementApplyPageModule } from './apply/procurementApply/procurementApply.module';
 import { StationeryApplyPageModule } from './apply/stationeryApply/stationeryApply.module';
-
 
 @NgModule({
   declarations:[
