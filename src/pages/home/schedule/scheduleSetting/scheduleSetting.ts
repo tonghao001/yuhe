7a3ae4f3be0656f4bb8864e83332d3f2188eb5a3
indation_elina_guo <elina.guo@indation.com>
2018-12-03 16:39:35 +0800

    添加日程安排

--- src/pages/home/schedule/scheduleSetting/scheduleSetting.ts
+++ src/pages/home/schedule/scheduleSetting/scheduleSetting.ts
@@ -8,11 +8,39 @@ import { Component } from '@angular/core';
   templateUrl: 'scheduleSetting.html'
 })
 export class ScheduleSettingPage {
+  list;
+
   constructor(private navCtrl: NavController) {
+    this.list = {
+      today: [
+        {
+          id: 1,
+          time: new Date('2018-10-10 18:30'),
+          scheduleTitle: '定制服装'
+        },
+        {
+          id: 2,
+          time: new Date('2018-10-10 8:30'),
+          scheduleTitle: '表演集合，体育场集合'
+        }
+      ],
+      tomorrow: [
+        {
+          id: 1,
+          time: new Date('2018-10-10 18:30'),
+          scheduleTitle: '定制服装'
+        },
+        {
+          id: 2,
+          time: new Date('2018-10-10 8:30'),
+          scheduleTitle: '表演集合，体育场集合'
+        }
+      ]
+    };
   }
 
   goToPage(pageName, id) {
-    pageName = pageName || 'app-home-childCheckList';
+    pageName = pageName || 'app-home-scheduleDetail';
     console.log('id:', id);
     this.navCtrl.push(pageName, { id: id });
   }
