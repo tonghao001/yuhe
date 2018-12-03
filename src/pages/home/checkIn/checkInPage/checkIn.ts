7d6cb27c488f03aca278c98257a8d8b76593c97f
刘聪 <conorliu@163.com>
2018-12-03 21:02:44 +0800

    update: 时间

--- src/pages/home/checkIn/checkInPage/checkIn.ts
+++ src/pages/home/checkIn/checkInPage/checkIn.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NavParams, IonicPage, NavController } from "ionic-angular";
+import { setInterval } from "timers";
 
 @IonicPage({
     name: "app-home-checkIn",
@@ -12,11 +13,14 @@ import { NavParams, IonicPage, NavController } from "ionic-angular";
 
 export class CheckInPage {
     user;
+    today = new Date();
+    nowTime = new Date();
     constructor(params: NavParams, public nav: NavController) {
         this.user = {
             name: '小肉丸',
             team: '考勤组: 技术部',
         }
+        setInterval(this.timerRun, 1000)
     }
 
     attendanceRecord() {
@@ -29,5 +33,9 @@ export class CheckInPage {
 
     reloadLocation() {
 
+    }
+
+    timerRun() {
+       this.nowTime = new Date();
     }
 }
