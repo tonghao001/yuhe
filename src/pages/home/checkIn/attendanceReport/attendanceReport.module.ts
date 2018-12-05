892f88988dd2c4b638e99c9124b1e53df96cbd08
indation_elina_guo <elina.guo@indation.com>
2018-12-05 16:36:16 +0800

    添加考勤报表中日历

--- src/pages/home/checkIn/attendanceReport/attendanceReport.module.ts
+++ src/pages/home/checkIn/attendanceReport/attendanceReport.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { AttendanceReportPage } from "./attendanceReport";
 
+import {CalendarModule} from 'primeng/calendar';
+
 
 @NgModule({
   declarations: [
@@ -9,7 +11,8 @@ import { AttendanceReportPage } from "./attendanceReport";
   ],
   
   imports: [
-    IonicPageModule.forChild(AttendanceReportPage)
+    IonicPageModule.forChild(AttendanceReportPage),
+    CalendarModule
   ],
   providers: [
     
--- src/pages/home/checkIn/attendanceReport/attendanceReport.ts
+++ src/pages/home/checkIn/attendanceReport/attendanceReport.ts
@@ -10,12 +10,101 @@ import { Component } from '@angular/core';
 export class AttendanceReportPage {
   constructor(private navCtrl: NavController) {
     this.currentReport = 'day';
+
+
+    this.calendar = {
+      weekNames: ['一', '二', '三', '四', '五', '六', '日'],
+      now: new Date(),
+      dates: [],
+      init: ()=>{
+        let now = new Date();
+        this.calendar.now = now;
+        let currentDate = now.getDate();
+        //let currentWeekday = now.getDay()||7;//获取今天周几,getDay()返回值是 0（周日） 到 6（周六） 之间的一个整数。0||7为7，即weekday的值为1-7
+        let firstWeekdayOfCurrentMonth = new Date(new Date().setDate(1)).getDay() || 7;//本月第一天为周几
+        this.calendar.dates = [];
+        console.log('firstWeekdayOfCurrentMonth:', firstWeekdayOfCurrentMonth);
+        for(let i = 1; i < firstWeekdayOfCurrentMonth; i++){
+          this.calendar.dates.push({number: ''});//本月第一天为周几，前面补空
+        }
+        console.log('dates:',this.calendar.dates);
+
+        let totalDayCountThisMonth = this.getDayCount();
+        console.log('totalDayCountThisMonth:', totalDayCountThisMonth);
+        for(let i = 1; i <= totalDayCountThisMonth; i++){
+          this.calendar.dates.push({number: i, active: false, today: currentDate === i, extra: {}});
+        }
+      },
+      toggleDate: (date)=>{
+          date.active = !date.active;
+      },
+      currentActiveDate: null,
+      changeCurrentActive: (date)=>{
+        if(!date.number){
+          return;
+        }
+        if(this.calendar.currentActiveDate){
+          this.calendar.currentActiveDate.active = false;
+        }
+        date.active = !date.active;
+        this.calendar.currentActiveDate = date;
+      }
+    };
+    this.calendar.init();
   }
 
 
-
+  value: Date;
   currentReport;
+  calendar;
 
+
+
+  //判断是否是闰年
+  isLeapYear(year){
+      if (year % 100 == 0 && year % 400 == 0){
+          return true;
+      }else if (year % 100 != 0 && year % 4==0){
+          return true;
+      }
+      return false;
+  }
+
+  //得到某月多少天
+  getDaysOfMonth(isLeapYear,month){
+      let days=0;
+      switch (month){
+          case 1:
+          case 3:
+          case 5:
+          case 7:
+          case 8:
+          case 10:
+          case 12:
+              days=31;
+              break;
+          case 4:
+          case 6:
+          case 9:
+          case 11:
+              days=30;
+              break;
+          case 2:
+              if (isLeapYear){
+                  days=29;
+              }else{
+                  days=28;
+              }
+      }
+      return days;
+  }
+
+  getDayCount(){
+    let now  = new Date();
+    let isLeapYear = this.isLeapYear(now.getFullYear());
+    let month = now.getMonth() + 1;
+    return this.getDaysOfMonth(isLeapYear, month);
+  }
 
   goToRankPage(): void {
     this.navCtrl.push('app-home-attendance-rank');
