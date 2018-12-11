a0fc4585f1013d4f76bce9638aab08c297dcca65
indation_elina_guo <elina.guo@indation.com>
2018-12-11 14:14:15 +0800

    Modify the signout method

--- src/network/user.network.ts
+++ src/network/user.network.ts
@@ -13,6 +13,6 @@ export class UserNetwork {
     return this.http.postForm('/yh_YEManager/app/login/getSMSCode', data);
   }
   logout(){
-    return this.http.get('/yh_YEManager/app/login/signOut');
+    return this.http.postForm('/yh_YEManager/app/login/signOut');
   }
 }
