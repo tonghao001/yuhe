aec33ac6a04f2ece2ad1f1c1b2afb22f235d3ec5
indation_elina_guo <elina.guo@indation.com>
2018-12-26 10:46:56 +0800

    Complete the student detail

--- src/pages/home/childManage/studentDetail/studentDetail.ts
+++ src/pages/home/childManage/studentDetail/studentDetail.ts
@@ -22,23 +22,23 @@ export class StudentDetailPage {
     this.className = params.data.className;
 
     this.student = {
-      code: 'student001',
-      name: '吴鑫桐',
-      className: this.className,
-      age: 4,
-      birthday: '1996-05',
-      address: '上海浦东新区X座11号',
-      family: [{
-        name: '王刚',
-        relation: '爸爸',
-        mobile: '12893889111'
-      }, {
-        name: '王芳',
-        relation: '妈妈',
-        mobile: '18321110999'
-      }],
-      cardNumber: '12345',
-      schoolDate: '2015-10'
+      // code: 'student001',
+      // name: '吴鑫桐',
+      // className: this.className,
+      // age: 4,
+      // birthday: '1996-05',
+      // address: '上海浦东新区X座11号',
+      // family: [{
+      //   name: '王刚',
+      //   relation: '爸爸',
+      //   mobile: '12893889111'
+      // }, {
+      //   name: '王芳',
+      //   relation: '妈妈',
+      //   mobile: '18321110999'
+      // }],
+      // cardNumber: '12345',
+      // schoolDate: '2015-10'
     };
     this.getStudentDetail();
   }
@@ -47,7 +47,7 @@ export class StudentDetailPage {
     this.classNetwork.getStudentDetail({ childId: this.studentId })
       .subscribe((detail: {
         xh: string, csrq: string, age: number, jg: string,
-        xm: string, zp: string, jskh: string, ryrq: string,
+        xm: string, zp: string, jskh: string, ryrq: string, statusValue: string,
         familyRels: [{ roleName: string, zbmc: string, zblxdh: string }]
       }) => {
         console.log(detail);
@@ -63,6 +63,7 @@ export class StudentDetailPage {
           this.student.cardNumber = detail.jskh;
           this.student.age = detail.age;
           this.student.photo = HTTP_URL.MAIN + '/images/' + detail.zp;
+          this.student.statusValue = detail.statusValue;
         }
       }, err => {
         this.toastService.show(err.message || '获取学生信息失败！');
