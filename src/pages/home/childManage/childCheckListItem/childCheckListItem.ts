1c097c902514495cf258cdd8f2f51679a02a4962
indation_elina_guo <elina.guo@indation.com>
2018-12-27 15:44:27 +0800

    Fix bug

--- src/pages/home/childManage/childCheckListItem/childCheckListItem.ts
+++ src/pages/home/childManage/childCheckListItem/childCheckListItem.ts
@@ -23,39 +23,6 @@ export class ChildCheckListItem {
     console.log('classId:', this.classId);
 
     this.getStudentAttendanceList();
-    // this.list = [
-    //   {
-    //     name: '朱晓杰',
-    //     status: 'inSchool',
-    //     statusString: '在校',
-    //     leaveCount: 54,
-    //     signCount: 54,
-    //     noSignedCount: 0
-    //   },{
-    
-    //     name: '胡乐乐',
-    //     status: 'inSchool',
-    //     statusString: '在校',
-    //     leaveCount: 54,
-    //     signCount: 54,
-    //     noSignedCount: 0
-    //   },{
-    //     name: '朱珠',
-    //     status: 'leave',
-    //     statusString: '请假',
-    //     leaveCount: 54,
-    //     signCount: 54,
-    //     noSignedCount: 0
-    //   },{
-    
-    //     name: '张小子',
-    //     status: 'inSchool',
-    //     statusString: '在校',
-    //     leaveCount: 54,
-    //     signCount: 54,
-    //     noSignedCount: 0
-    //   }
-    // ];
   }
 
   getStudentAttendanceList() {
