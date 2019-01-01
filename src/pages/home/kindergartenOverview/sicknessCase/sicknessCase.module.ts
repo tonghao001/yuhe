a89de38816b268e010580b3dde22af9ec765ddf9
梅志威 <meizhiwei@tezign.com>
2019-01-01 23:10:48 +0800

    疾病情况

--- src/pages/home/kindergartenOverview/sicknessCase/sicknessCase.module.ts
+++ src/pages/home/kindergartenOverview/sicknessCase/sicknessCase.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { SicknessCasePage } from "./sicknessCase";
 import { ChartModule } from 'angular-highcharts';
+import { ComponentsModule } from "../../../../components/components.module";
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { ChartModule } from 'angular-highcharts';
   ],
   imports: [
     IonicPageModule.forChild(SicknessCasePage),
-    ChartModule
+    ChartModule,
+    ComponentsModule
   ],
   entryComponents: [
     SicknessCasePage
--- src/pages/home/kindergartenOverview/sicknessCase/sicknessCase.ts
+++ src/pages/home/kindergartenOverview/sicknessCase/sicknessCase.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 import { KindergartenOverviewNetwork } from '../../../../network/kindergartenOverview.network';
 import { formatDate } from '../../../../network/http';
+import { ToastService } from '../../../../service/toast.service';
 
 @IonicPage({
   name: 'app-home-sickness-case'
@@ -13,41 +14,23 @@ import { formatDate } from '../../../../network/http';
 export class SicknessCasePage {
   timeString: 'day';
   chart: Chart;
-  startDate: Date;
   categoryList: any[];
+
+  currentTag: string;
+  startDateString: string;
+  endDateString: string;
+  startDateFilter: Date;
+  endDateFilter: Date;
 
   constructor(
     public navCtrl: NavController,
+    public toastService: ToastService,
     private kindergartenOverviewNetwork: KindergartenOverviewNetwork
   ) {
-    this.startDate = new Date(formatDate('2018/10/1', 'yyyy/MM/dd'));
-    this.startDate.setDate(1);
     this.categoryList = [];
-  }
-
-  ionViewDidEnter() {
-    this.refreshData();
-  }
-
-  refreshData() {
-    let params = {
-      startDate: formatDate(this.startDate, 'yyyy-MM-dd'),
-      endDate: formatDate(this.getEndDate(), 'yyyy-MM-dd')
-    };
-    this.updateChart(params);
-  }
-
-  getEndDate() {
-    return new Date(new Date(this.startDate).setMonth(this.startDate.getMonth() + 1) - 1000);
-  }
-
-  goPrevMonth() {
-    this.startDate = new Date(this.startDate.setMonth(this.startDate.getMonth() - 1));
-    this.refreshData();
-  }
-  goNextMonth() {
-    this.startDate = new Date(this.startDate.setMonth(this.startDate.getMonth() + 1));
-    this.refreshData();
+    this.startDateString = formatDate(new Date(), 'yyyy-MM-dd');
+    this.endDateString = formatDate(new Date(), 'yyyy-MM-dd');
+    this.changeTag('day');
   }
 
   updateChart = (params: any) => {
@@ -113,20 +96,124 @@ export class SicknessCasePage {
       });
   }
 
+  changeTag(tag: string) {
+    console.log('tag:', tag);
+    if (tag === this.currentTag) {
+      return;
+    }
+    this.currentTag = tag;
+    switch (this.currentTag) {
+      case 'month':
+        console.log('init month');
+        this.loadMonthChart(new Date(formatDate(new Date(), 'yyyy-MM-1')));
+        break;
+      case 'season':
+        console.log('init season');
+        let currentSeason = Math.floor((new Date().getMonth() + 3) / 3);//第几季度
+        let theCurrentSeasonFirstDayString = [new Date().getFullYear(), (3 * currentSeason - 2), 1].join('/');
+        this.loadSeasonChart(new Date(theCurrentSeasonFirstDayString));//默认为当季度第一天
+        break;
+      case 'year':
+        console.log('init year');
+        this.loadYearChart(new Date(formatDate(new Date(), 'yyyy')));
+        break;
+      case 'day':
+      default:
+        this.loadDateChart();
+        break;
+    }
+  }
+
+  request(startDate, endDate) {
+    this.updateChart({
+      startDate,
+      endDate
+    });
+  }
+
+  loadDateChart() {
+    this.startDateFilter = new Date(this.startDateString);
+    this.endDateFilter = new Date(this.endDateString);
+    if (this.endDateFilter.getTime() < this.startDateFilter.getTime()) {
+      return this.toastService.show('开始时间不能大于结束时间！');
+    }
+    this.request(this.startDateString, this.endDateString);
+  }
+
+
+  loadMonthChart(date) {
+    let startString = formatDate(date, 'yyyy-MM-dd');
+    let nextMonth = new Date(new Date(startString).setMonth(date.getMonth() + 1));
+    let end = new Date(nextMonth.setDate(nextMonth.getDate() - 1));
+    let endString = formatDate(end, 'yyyy-MM-dd');
+
+
+    //用于条件筛选
+    this.startDateFilter = date;
+    this.endDateFilter = end;
+
+    console.log('start:', startString, ',end:', endString);
+    this.request(startString, endString);
+  }
+  nextMonth(date) {
+    console.log('next month:');
+    this.loadMonthChart(date);
+  }
+  prevMonth(date) {
+    console.log('prev month:', date);
+    this.loadMonthChart(date);
+  }
+
+  loadSeasonChart(date) {
+    let startString = formatDate(date, 'yyyy-MM-dd');
+    let nextSeason = new Date(new Date(startString).setMonth(date.getMonth() + 3));
+    let end = new Date(nextSeason.setDate(nextSeason.getDate() - 1));
+    let endString = formatDate(end, 'yyyy-MM-dd');
+
+    //用于条件筛选
+    this.startDateFilter = date;
+    this.endDateFilter = end;
+
+    console.log('start:', startString, ',end:', endString);
+    this.request(startString, endString);
+  }
+  nextSeason(date) {
+    console.log('next Season:');
+    this.loadSeasonChart(date);
+  }
+  prevSeason(date) {
+    console.log('prev Season:');
+    this.loadSeasonChart(date);
+  }
+
+  loadYearChart(date) {
+    let endString = `${date.getFullYear() + '-12-31'}`;
+    console.log('start:', formatDate(date, 'yyyy-MM-dd'), ',end:', endString);
+
+    //用于条件筛选
+    this.startDateFilter = date;
+    this.endDateFilter = new Date(endString);
+
+    this.request(formatDate(date, 'yyyy-MM-dd'), endString);
+  }
+  nextYear(date) {
+    console.log('next year:', date);
+    this.loadYearChart(date);
+  }
+  prevYear(date) {
+    console.log('prev year:');
+    this.loadYearChart(date);
+  }
+
 
   goToPage(pageName, id) {
     pageName = pageName || 'app-home-sickness-case-list';
     console.log('id:', id);
     this.navCtrl.push(pageName, {
-      sickName:id,
-      startDate: formatDate(this.startDate,'yyyy-MM-dd'),
-      endDate: formatDate(this.getEndDate(),'yyyy-MM-dd')
+      sickName: id,
+      startDate: formatDate(this.startDateFilter, 'yyyy-MM-dd'),
+      endDate: formatDate(this.endDateFilter, 'yyyy-MM-dd')
     });
-  }
-
-  selectTime(event) {
-    console.log(event.value);
-    this.timeString = event.value;
   }
 
 }
