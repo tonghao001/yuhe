7734fb49b172e1c53d9c29cc441fa955db16bf9a
indation_elina_guo <elina.guo@indation.com>
2019-01-01 18:47:12 +0800

    Add new Schedule

--- src/pages/home/schedule/newSchedule/newSchedule.ts
+++ src/pages/home/schedule/newSchedule/newSchedule.ts
@@ -1,6 +1,9 @@
 import { IonicPage, NavController } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { ConfirmService } from '../../../../service/confirm.service';
+import { formatDate } from '../../../../network/http';
+import { ToastService } from "../../../../service/toast.service";
+import { ScheduleNetwork } from "../../../../network/schedule.network";
 
 @IonicPage({
   name: 'app-home-newSchedule'
@@ -9,8 +12,23 @@ import { ConfirmService } from '../../../../service/confirm.service';
   templateUrl: 'newSchedule.html'
 })
 export class NewSchedulePage {
-  constructor(private navCtrl: NavController,
-    private confirmService: ConfirmService) {
+
+  schedule: any;
+  constructor(
+    private navCtrl: NavController,
+    private confirmService: ConfirmService,
+    private toastService: ToastService,
+    private scheduleService: ScheduleNetwork
+  ) {
+
+    let now = new Date();
+    this.schedule = {
+      startDateString: formatDate(now, 'yyyy-MM-dd'),
+      startTimeString: formatDate(now, 'HH:mm'),
+      title: '',
+      content: '',
+      remark: ''
+    };
   }
 
   save(): void {
@@ -24,10 +42,43 @@ export class NewSchedulePage {
         },
         {
           handler: () => {
-            this.navCtrl.push('app-home-scheduleSetting');
+            this.saveSchedule();
+            // this.navCtrl.push('app-home-scheduleSetting');
           }
         }
       ]
     })
+  }
+
+
+  saveSchedule() {
+    if (!this.schedule.title) {
+      return this.toastService.show('标题必填');
+    }
+
+    if (!this.schedule.content) {
+      return this.toastService.show('描述必填');
+    }
+
+    this.scheduleService.saveSchedule({
+      title: this.schedule.title,
+      summary: this.schedule.content,
+      scheduleDate: this.schedule.startDateString,
+      beginTime: this.schedule.startTimeString + ':00'
+    })
+      .subscribe((result: { status: number }) => {
+        if (result.status === 0) {
+          this.navCtrl.push('app-home-scheduleSetting');
+        } else {
+          this.toastService.show('保存失败！');
+        }
+      }, err => {
+        this.toastService.show('保存出错！');
+      });
+
+
+
+
+
   }
 }
