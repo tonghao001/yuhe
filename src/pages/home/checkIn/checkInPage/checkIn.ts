28ed8dc7f56e020211a4c1b27d8cf8abcf140fe2
刘聪 <conorliu@163.com>
2019-01-01 11:07:58 +0800

    update: 修复定位

--- src/pages/home/checkIn/checkInPage/checkIn.ts
+++ src/pages/home/checkIn/checkInPage/checkIn.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component } from "@angular/core";
 import { NavParams, IonicPage, NavController } from "ionic-angular";
 import { setInterval } from "timers";
@@ -15,15 +16,19 @@ import { userInfo } from "os";
 
 export class CheckInPage {
     user;
-    companyLocation: [];
-    userLocation: [];
+    companyLocation = [];
+    userLocation = [];
     today:any = Date.now();//或者today:any = new Date();
     timer:any;
+    isChenckIn: boolean = true;
+    checkData: any = {};
+
     constructor(
          params: NavParams,
          public nav: NavController,
          public checkNetWork: CheckNetwork,
          private storage: StorageService,
+         public datePipe: DatePipe,
         ) {
         let loginInfo = this.storage.get(STORAGE_KEY.LOGIN_INFO);
         console.log(userInfo);
@@ -31,12 +36,28 @@ export class CheckInPage {
             name: loginInfo.username,
             team: '考勤组: 技术部',
         }
+
+        let middle = new Date("12:00:00");
+        let now = new Date();
+
+        if (now > middle) {
+            this.isChenckIn = false;
+        }
+
         this.timer=setInterval(()=> {
             this.today = Date.now();// 或者this.today = new Date();
         }, 1000)
 
-        // this.checkNetWork.getCompanyLocation
-
+        this.checkNetWork.getCompanyLocation().subscribe(
+            (data: any) => {
+              console.log(data);
+              if (data) {
+                this.companyLocation = [data.latitude, data.longitude];
+              }
+            },
+            error => {
+              console.log(error);
+            });
     }
     ngOnDestroy(){
         clearInterval(this.timer);
@@ -47,7 +68,22 @@ export class CheckInPage {
     }
 
     checkInClick() {
-        
+        if (this.userLocation.length < 2) {
+            return;
+        }
+        this.checkNetWork.checkIn({
+            latitude: this.userLocation[0],
+            longitude: this.userLocation[1],
+        }).subscribe(
+            (data: any) => {
+              console.log(data);
+              if (data) {
+                this.companyLocation = [data.latitude, data.longitude];
+              }
+            },
+            error => {
+              console.log(error);
+            });
     }
 
     reloadLocation() {
